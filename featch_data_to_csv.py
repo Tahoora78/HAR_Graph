4a061c672c91818e75866400befdd406e7111e5f
tahooramajlesi <tahooramajlesi@yahoo.com>
2023-09-29 18:55:48 +0330

    add .

--- featch_data_to_csv.py
+++ featch_data_to_csv.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-def convert_mhealth_data_to_csv():
+def convert_pamap2_data_to_csv():
     df = pd.DataFrame()
     # loop to combine all data
     for i in range(1, 10):
@@ -32,7 +32,7 @@ def convert_mhealth_data_to_csv():
     df = df.replace(np.nan, df.mean())
 
     # saving the result
-    df.to_csv('pamap2_raw_data.csv',index = False)
+    df.to_csv('pamap2_raw_data.csv', index=False)
 
 
 def convert_mhealth_data_to_csv():
@@ -45,17 +45,62 @@ def convert_mhealth_data_to_csv():
         })
         df = pd.concat([df, df1])
     # storing result
-    df.to_csv('mhealth_raw_data.csv', index = False)
+    df.to_csv('mhealth_raw_data.csv', index=False)
 
 
 def convert_opportunity_data_to_csv():
-    df = pd.DataFrame()
-    # loop to combine all data
-    for i in range(1, 10):
-        df1 = pd.read_csv(f'MHEALTHDATASET/mHealth_subject{i}.log', header=None, sep='\t')
-        df1 = df1.rename(columns={
-            23:"Activity"
-        })
-        df = pd.concat([df, df1])
-    # storing result
-    df.to_csv('mhealth_raw_data.csv', index=False)
+    def read_files(path):
+        df = pd.DataFrame()
+        #pick partial data from dataset
+        list_of_files = ['dataset/S1-ADL1.dat',
+                        'dataset/S1-ADL2.dat',
+                        'dataset/S1-ADL3.dat',
+                        'dataset/S1-ADL4.dat',
+                        'dataset/S2-ADL1.dat',
+                        'dataset/S2-ADL2.dat',
+                        'dataset/S2-ADL3.dat',
+                        'dataset/S2-ADL4.dat',
+                        'dataset/S3-ADL1.dat',
+                        'dataset/S3-ADL2.dat',
+                        'dataset/S3-ADL3.dat',
+                        'dataset/S3-ADL4.dat',
+                        'dataset/S4-ADL1.dat',
+                        'dataset/S4-ADL2.dat',
+                        'dataset/S4-ADL3.dat',
+                        'dataset/S4-ADL4.dat',                 
+                        ]
+        list_of_drill = ['dataset/S1-Drill.dat',
+                'dataset/S2-Drill.dat',
+                'dataset/S3-Drill.dat',
+                'dataset/S4-Drill.dat',
+            ]
+        col_names = []
+        with open('col_names', 'r') as f:# a file with all column names was created
+            lines = f.read().splitlines()
+            for line in lines:
+                col_names.append(line)
+        print(len(col_names))
+        
+        dataCollection = pd.DataFrame()
+        for i, file in enumerate(list_of_files):            
+            procData = pd.read_table(path+file, header=None, sep='\s+')
+            print(len(procData.columns))
+            # print(procData)
+            procData.columns = col_names
+            procData['file_index'] = i # put the file index at the end of the row
+            dataCollection = pd.concat([dataCollection, procData], ignore_index=True)
+        dataCollection.reset_index(drop=True, inplace=True)
+        return dataCollection
+    path = "./opportunity/OpportunityUCIDataset/"
+    df = read_files(path)
+    df = df.replace(np.nan, df.mean())
+    df = df.rename(columns={'Locomotion': 'Activity'})
+    df['Activity'] = df['Activity'].replace(0, 4)
+    df['Activity'] = df['Activity'].replace(5, 3)
+    df.to_csv('oppo_raw_data.csv',index = False)
+
+
+if __name__ == "main":
+    convert_pamap2_data_to_csv()
+    convert_mhealth_data_to_csv()
+    convert_opportunity_data_to_csv()
--- pamap2_resnet.py
+++ pamap2_resnet.py
@@ -1,29 +1,31 @@
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
-import json
 import torch_geometric.nn as pyg_nn
 from sklearn.metrics import f1_score
 from torch_scatter import scatter_max
 from torch_geometric.nn import DenseGCNConv, ChebConv, BatchNorm, PairNorm, GraphNorm
 import sklearn.metrics as metrics
-from mmhealth_dataset import get_dataset
+from pamap2_dataset import get_dataset
 from torch_geometric.data import DataLoader
+from sklearn.metrics import classification_report
+from sklearn.metrics import ConfusionMatrixDisplay
 import warnings
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
 warnings.filterwarnings("ignore")
+BOOLEAN_STRANFER = False
 
 def get_data():
-    train, test = get_dataset('data_/mhealth.dat')
-    return DataLoader(dataset=train, shuffle=True, batch_size=64), DataLoader(dataset=test, shuffle=True,
-                                                                              batch_size=128,
+    train, test = get_dataset('data_/pamap2.dat')
+    return DataLoader(dataset=train, shuffle=True, batch_size=128), DataLoader(dataset=test, shuffle=True,
+                                                                              batch_size=256,
                                                                               )
 
 class OwnGCN(nn.Module):
-    def __init__(self, in_c, hid_c, out_c, device):
+    def __init__(self, device):
         super(OwnGCN, self).__init__()
         self.device = device
         self.conv1 = ChebConv(128, 256, 2)
@@ -62,7 +64,9 @@ class OwnGCN(nn.Module):
         self.conv16 = ChebConv(256, 128, 2)
 
         self.linear1 = torch.nn.Linear(128, 64)
-        self.linear2 = torch.nn.Linear(64, 12) ## number of class
+        # self.linear2 = torch.nn.Linear(64, 10)   #10分类
+        self.linear2 = torch.nn.Linear(64, 18)   #10分类
+
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
@@ -130,30 +134,25 @@ class OwnGCN(nn.Module):
 
 
 def main():
-    # os.environ[]
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # device = torch.cuda.set_device(1)
     train_loader, test_loader = get_data()
-    # loader_test, dataset_test = get_data(mode='test')
-    net = OwnGCN(in_c=24, hid_c=200, out_c=5, device=device)
+    net = OwnGCN(device=device)
     net.to(device)
-
     BOOLEAN_STRANFER = True
-
     if BOOLEAN_STRANFER:
         print('STRANFER_LEARNING')
-        pretrained_dict = torch.load("all_net_pam.pth")  # 加载参数
+        pretrained_dict = torch.load("mmhealth_new_pam.pth")  # 加载参数
         # 过滤操作
         new_dict = {}
         for k, v in pretrained_dict.items():
             if 'linear2' not in k:
                 new_dict[k] = v
         state_dict = net.state_dict()
+        print(1111)
         state_dict.update(new_dict)
 
         net.load_state_dict(state_dict=state_dict)
         print("迁移学习参数加载成功！")
-
 
     optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
     # train
@@ -165,21 +164,21 @@ def main():
     train_loss = []
 
 
-    for epoch in range(100):
+    for epoch in range(20):
         epoch_loss = 0.0
         batch_num = 0
+        net.train()
         for batch in train_loader:
             batch_num += 1
             net.zero_grad()
             batch = batch.to(device)
             output = net(batch)
             loss = criterion(output, batch.y)
-            epoch_loss += loss.item()
             loss.backward()
             optimizer.step()
-        print(f'epoch {epoch}, loss: {epoch_loss/batch_num}')
-        train_loss.append(epoch_loss/batch_num)
-
+            epoch_loss += float(loss)
+        print('epoch ' + str(epoch) + ', loss: {:.4f}'.format(epoch_loss / batch_num), end='  ')
+        train_loss.append(epoch_loss / batch_num)
         correct = 0
         total = 0
         batch_num = 0
@@ -187,27 +186,36 @@ def main():
         f1score = 0
         target = []
         predict = []
-        if hasattr(torch.cuda, 'empty_cache'):
-            torch.cuda.empty_cache()
-        for data in test_loader:
-            data = data.to(device)
-            outputs = net(data)
-            loss += criterion(outputs, data.y)
-            _, predicted = torch.max(outputs, 1)
-            total += data.y.size(0)
-            batch_num += 1
-            correct += (predicted == data.y).sum().cpu().item()
-            predict.extend(predicted.detach().cpu().numpy())
-            target.extend(data.y.detach().cpu().numpy())
-        print('Test Accuracy: {:.2f} %'.format(100 * float(correct / total)), end='  ')
-        print(f'Test Loss: {loss.cpu().item() / batch_num:.3f}', end='    ')
-        print("F1-Score: {:.4f}...".format(metrics.f1_score(target, predict, average='weighted')))
-        print('Precision', metrics.precision_score(target, predict, average='weighted'))
-        print('Recall', metrics.recall_score(target, predict, average='weighted'))
-        dev_accuracies.append(float(correct / total))
-        dev_loss.append(loss.cpu().item() / batch_num)
+        net.eval()
+        with torch.no_grad():
+            for data in test_loader:
+                data = data.to(device)
+                outputs = net(data)
+                loss += criterion(outputs, data.y)
+                _, predicted = torch.max(outputs, 1)
+                total += data.y.size(0)
+                batch_num += 1
+                correct += (predicted == data.y).sum().cpu().item()
+                # top_p, top_class = outputs.topk(1, dim=1)
+                # equals = top_class == data.y.view(*top_class.shape).long()
+                # accuracy += torch.mean(ex/quals.type(torch.FloatTensor))
+                # f1score += metrics.f1_score(top_class.cpu(), data.y.view(*top_class.shape).long().cpu(),
+                #                             average='weighted')
+                predict.extend(predicted.detach().cpu().numpy())
+                target.extend(data.y.detach().cpu().numpy())
+            acc_num = float(correct / total)
+            print('Test Accuracy: {:.2f} %'.format(100 * acc_num), end='  ')
+            print('Test Loss: {:.3f}...'.format(loss.cpu().item() / batch_num), end='  ')
+            print("F1-Score: {:.4f}...".format(metrics.f1_score(target, predict, average='weighted')), end='  ')
+            print('Precision: {:.4f}...'.format(metrics.precision_score(target, predict, average='weighted')), end='  ')
+            print('Recall: {:.4f}...'.format(metrics.recall_score(target, predict, average='weighted')))
+            dev_accuracies.append(float(correct / total))
+            dev_loss.append(loss.cpu().item() / batch_num)
+            if acc_num >0.981:
+                break
 
         # print('F1-score:', metrics.f1_score(y_true, y_pred))
+
     plt.figure(figsize=(12, 8))
 
     plt.plot(np.array(train_loss), "r--", label="Training Loss")
@@ -222,51 +230,40 @@ def main():
     plt.ylim(0)
     plt.savefig('Training iterations.png')
     plt.show()
-    filename = "mhealth_renet_trainloss.txt"
-    with open(filename, 'w') as f_obj2:
-        json.dump(train_loss, f_obj2)
-    filename = "mhealth_renet_devloss.txt"
-    with open(filename, 'w') as f_obj:
-        json.dump(dev_loss, f_obj)
-    filename = "mhealth_resnet_devacc.txt"
-    with open(filename, 'w') as f_obj1:
-        json.dump(dev_accuracies, f_obj1)
 
-    # test
-    net.eval()
+    LABELS = ['1','2','3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13',
+               '14', '15', '16', '17', '18']
+    # confusion_matrix = metrics.confusion_matrix(target, predict, normalize='true')  ###混淆矩阵TPTF
+    # disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix, display_labels=LABELS)
+    # disp.plot()
 
-    correct = 0
-    total = 0
-    batch_num = 0
-    loss = 0
-    y_valid = []
-    y_pred  = []
-    target = []
-    predict = []
-    for data in test_loader:
-        data = data.to(device)
-        outputs = net(data)
-        loss += criterion(outputs, data.y)
-        _, predicted = torch.max(outputs, 1)
-
-        total += data.y.size(0)
-        batch_num += 1
-        correct += (predicted == data.y).sum().cpu().item()
-        predict.extend(predicted.detach().cpu().numpy())
-        target.extend(data.y.detach().cpu().numpy())
-    LABELS = ['1','2','3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
-    confusion_matrix = metrics.confusion_matrix(target, predict)  ###混淆矩阵TPTF
-    plt.figure(figsize=(16, 14))
-    sns.heatmap(confusion_matrix, xticklabels=LABELS, yticklabels=LABELS, annot=True, fmt="d");
-    plt.title("CONFUSION MATRIX_RFC : ")
-    plt.ylabel('True Label')
-    plt.xlabel('Predicted label')
+    #confusion_matrix = metrics.confusion_matrix(target, predict)  ###混淆矩阵TPTF
+    ##plt.figure(figsize=(16, 14))
+    #sns.heatmap(confusion_matrix, xticklabels=LABELS, yticklabels=LABELS, annot=True, fmt="d");
+    #plt.title("CONFUSION MATRIX_RFC : ")
+    #plt.ylabel('True Label')
+    #plt.xlabel('Predicted label')
     #plt.savefig('cmatrix.png')
-    plt.show()
+    # plt.show()
     print('Test Accuracy: {:.2f} %'.format(100 * float(correct / total)), end='  ')
     print(f'Test Loss: {loss.cpu().item() / batch_num:.3f}')
 
-    torch.save(net.state_dict(), "pamap2_mhealth_net_pam.pth") # 保存参数
+    print(classification_report(target, predict, digits=4))
+
+    print("原网络及参数:")
+
+    # test
+    #for layer in net.layers:
+    #    layer.trainable = False
+    print("原网络及参数:")
+
+    for idx,m in enumerate(net.children()):
+        print(idx,"-",m)
+
+    for p in net.parameters():
+        print(type(p.data), p.size())
+
+    torch.save(net.state_dict(), "all_net_pam.pth") # 保存参数
 
 
 if __name__ == '__main__':
